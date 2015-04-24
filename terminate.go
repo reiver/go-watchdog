21ba5c2af900fbf4861e654321e498ec0fb0bb49
Charles Iliya Krempeaux <iamreiver@gmail.com>
2015-04-24 00:51:16 -0700

    added to terminate docs a bit

--- terminate.go
+++ terminate.go
@@ -12,6 +12,7 @@ type terminate struct {
 
 
 // Terminate is 1 of 3 methods needed by oneForOne to implement the Watcher interface.
+// Terminate is also 1 of 2 methods needed by oneForOne to implement the Toiler interface.
 func (dog *oneForOne) Terminate() {
 	done := make(chan struct{})
 
@@ -23,6 +24,7 @@ func (dog *oneForOne) Terminate() {
 }
 
 // Terminate is 1 of 3 methods needed by oneForAll to implement the Watcher interface.
+// Terminate is also 1 of 2 methods needed by oneForAll to implement the Toiler interface.
 func (dog *oneForAll) Terminate() {
 	done := make(chan struct{})
 
