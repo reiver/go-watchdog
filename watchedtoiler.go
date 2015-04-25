e3909229d96ad35830a922fa753d34d9245dd086
Charles Iliya Krempeaux <iamreiver@gmail.com>
2015-04-24 23:36:39 -0700

    reworded some documentation to make it clearer.

--- watchedtoiler.go
+++ watchedtoiler.go
@@ -6,9 +6,10 @@ package watchdog
 // A WatchedToiler is a Toiler (as it implements both the Terminate method and Toil method)
 // that is "binded" to a Watcher.
 //
-// This "binding" is expressed in 2 ways. #1: The WatchedToiler has a Watcher method that
-// will return the Watcher. And #2: The Toil method is run the underlying Toiler's Toil
-// method is a special way so that the Watcher is watching it (for panic()s).
+// This "binding" is expressed in 2 ways. #1 the WatchedToiler has a Watcher method that
+// will return the Watcher. And #2 when the Toil method of the WatchedToiler is invoked the
+// underlying Toiler's Toil method is invoked in a special way so that the Watcher is
+// watching it (for panic()s).
 type WatchedToiler interface {
 	Terminate()
 	Toil()
